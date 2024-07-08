e9a82fd53cbc3b37d73ae83046129555efcbae93
b4ry <barry90@interia.pl>
2024-07-08 15:11:56 +0200

    - add salt column db migration

--- Cerberus.DatabaseContext/Migrations/20240708130234_AddSaltColumnToUsersTable.Designer.cs
+++ Cerberus.DatabaseContext/Migrations/20240708130234_AddSaltColumnToUsersTable.Designer.cs
@@ -2,6 +2,7 @@
 using Cerberus.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cerberus.DatabaseContext.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240708130234_AddSaltColumnToUsersTable")]
+    partial class AddSaltColumnToUsersTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.6");
