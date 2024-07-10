5757db9dc0d104434b196dfc5fbc89d112466ae5
b4ry <barry90@interia.pl>
2024-07-10 12:57:49 +0200

    - add rate limit configuration comments

--- Cerberus.Api/ConfigurationSections/RateLimitConfigurationSection.cs
+++ Cerberus.Api/ConfigurationSections/RateLimitConfigurationSection.cs
@@ -1,11 +1,33 @@
 ﻿namespace Cerberus.Api.Options
 {
+    /// <summary>
+    /// Class encapsulating rate limit configuration section coming from IConfiguration.
+    /// </summary>
     public class RateLimitConfigurationSection
     {
+        /// <summary>
+        /// Rate limiting algorithm name
+        /// </summary>
         public string PolicyName { get; set; }
+
+        /// <summary>
+        /// Number of requests in a window, before blocking
+        /// </summary>
         public int PermitLimit { get; set; }
+
+        /// <summary>
+        /// Size of a window 
+        /// </summary>
         public int Window { get; set; }
+
+        /// <summary>
+        /// Maximum number of segments a window is divided to (used for sliding algorithm)
+        /// </summary>
         public int SegmentsPerWindow { get; set; }
+
+        /// <summary>
+        /// How many requests to queue when a window is filled
+        /// </summary>
         public int QueueLimit { get; set; }
     }
 }
