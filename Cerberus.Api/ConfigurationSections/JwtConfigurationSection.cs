5562b2c9b91fb6d20fa14177eb9fd712e817b326
b4ry <barry90@interia.pl>
2024-07-10 12:53:36 +0200

    - add jwt configuration section comments

--- Cerberus.Api/ConfigurationSections/JwtConfigurationSection.cs
+++ Cerberus.Api/ConfigurationSections/JwtConfigurationSection.cs
@@ -1,8 +1,18 @@
 ﻿namespace Cerberus.Api.ConfigurationSections
 {
+    /// <summary>
+    /// Class encapsulating JWT configuration section coming from IConfiguration.
+    /// </summary>
     public class JwtConfigurationSection
     {
+        /// <summary>
+        /// JWT key
+        /// </summary>
         public string Key { get; set; }
+
+        /// <summary>
+        /// JWT issuer
+        /// </summary>
         public string Issuer { get; set; }
     }
 }
