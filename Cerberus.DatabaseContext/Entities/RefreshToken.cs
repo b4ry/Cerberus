bfb6ae8b0263f20082245e4cc449bae2f67ff6d6
b4ry <barry90@interia.pl>
2024-11-14 10:52:23 +0100

    - add refresh token entity, - add migrations for the refresh tokens

---
+++ Cerberus.DatabaseContext/Entities/RefreshToken.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cerberus.DatabaseContext.Entities
+{
+    [Table("RefreshTokens")]
+    [PrimaryKey("Id")]
+    public class RefreshTokenEntity : BaseEntity
+    {
+        [Required]
+        public required string Id { get; set; }
+
+        [Required]
+        public required DateTime ValidUntil { get; set; }
+    }
+}
--- Cerberus.DatabaseContext/Migrations/20241114095103_AddRefreshTokensTable.Designer.cs
+++ Cerberus.DatabaseContext/Migrations/20241114095103_AddRefreshTokensTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Cerberus.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Cerberus.DatabaseContext.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241114095103_AddRefreshTokensTable")]
+    partial class AddRefreshTokensTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.6");
